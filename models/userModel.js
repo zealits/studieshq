48d48922dd31608f808a463b1bfdb6792d0bcfc3
Aniket Arjun Khillare <aniketkhillare17@gmail.com>
2024-08-13 14:42:45 +0530

    moved the folder accordingly for cicd

---
+++ models/userModel.js
@@ -0,0 +1,214 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
+
+const gigSchema = new mongoose.Schema({
+  gigId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Gig",
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  deadline: {
+    type: Date,
+  },
+  budget: {
+    type: Number,
+  },
+  status: {
+    type: String,
+    enum: ["applied", "allocated", "completed"],
+    required: true,
+  },
+  appliedAt: {
+    type: Date,
+    default: Date.now,
+  },
+  allocatedAt: Date,
+  completedAt: Date,
+  paymentStatus: {
+    type: String,
+    enum: ["not requested", "requested", "approved", "paid"],
+    default: "not requested",
+  },
+  giftCardOption: {
+    type: String,
+    // enum: ["visa", "master", "none"],
+  },
+  requestGiftCardAt: Date,
+  giftCardApprovedAt: Date,
+  giftCardPaidAt: Date,
+});
+
+const educationSchema = new mongoose.Schema({
+  college: {
+    type: String,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  specialization: {
+    type: String,
+    required: true,
+  },
+});
+
+
+const experienceSchema = new mongoose.Schema({
+  company: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: String,
+    required: true,
+  },
+  description: String,
+});
+
+const languageSchema = new mongoose.Schema({
+  language: {
+    type: String,
+    required: true,
+  },
+  proficiency: {
+    type: String,
+    required: true,
+  },
+});
+
+
+const skillSchema = new mongoose.Schema({
+  skill: {
+    type: String,
+    required: true,
+  },
+  proficiency: {
+    type: String,
+    required: true,
+  },
+});
+
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please Enter Your Name"],
+    maxLength: [30, "Name cannot exceed 30 characters"],
+    minLength: [4, "Name should have more than 4 characters"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please Enter Your Email"],
+    unique: true,
+    validate: [validator.isEmail, "Please Enter a valid Email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please Enter Your Password"],
+    minLength: [8, "Password should be greater than 8 characters"],
+    select: false,
+  },
+  firstName: {
+    type: String,
+    trim: true,
+  },
+  middleName: {
+    type: String,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    trim: true,
+  },
+  gender: {
+    type: String,
+  },
+  dateOfBirth: {
+    type: String,
+   
+  },
+  country: {
+    type: String,
+   
+    trim: true,
+  },
+  state: {
+    type: String,
+  
+    trim: true,
+  },
+  city: {
+    type: String,
+
+    trim: true,
+  },
+  contactNumber: {
+    type: String,
+    trim: true,
+  },
+  role: {
+    type: String,
+    default: "user",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  gigs: [gigSchema],
+  education: [educationSchema],
+  experience: [experienceSchema],
+  languages: [languageSchema],
+  skills: [skillSchema],
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+// JWT Tokens
+userSchema.methods.getJWTToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
+
+// Compare Password
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+// Generating Password Reset Token
+userSchema.methods.getResetPasswordToken = function () {
+  // Generating Token
+  const resetToken = crypto.randomBytes(20).toString("hex");
+
+  // Hashing and adding resetPasswordToken to userSchema
+  this.resetPasswordToken = crypto.createHash("sha256").update(resetToken).digest("hex");
+
+  this.resetPasswordExpire = Date.now() + 15 * 60 * 1000;
+
+  return resetToken;
+};
+
+module.exports = mongoose.model("User", userSchema);
---
+++ routes/userRoute.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const {
+  registerUser,
+  loginUser,
+  logout,
+  forgotPassword,
+  resetPassword,
+  getUserDetails,
+  updatePassword,
+  updateProfile,
+  getAllUser,
+  getSingleUser,
+  updateUserRole,
+  deleteUser,
+  applyForGig,
+  approveGig,
+  completeGig,
+  getAllGigsWithApplicants,
+  requestGiftCard, // New route for requesting a gift card
+  approveGiftCard,
+  sendTableDataEmail, // New route for admin to approve gift card requests
+  updateBasicInfo,
+  updateEducation,
+  updateExperience,
+  updateSkills,
+  updateLanguages,
+  sendGiftCard,
+} = require("../controllers/userController");
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
+const router = express.Router();
+
+router.route("/register").post(registerUser);
+router.route("/login").post(loginUser);
+
+router.route("/password/forgot").post(forgotPassword);
+router.route("/password/reset/:token").put(resetPassword);
+
+router.route("/logout").get(logout);
+router.route("/me").get(isAuthenticatedUser, getUserDetails);
+router.route("/password/update").put(isAuthenticatedUser, updatePassword);
+router.route("/me/update").put(isAuthenticatedUser, updateProfile);
+
+// New route for updating basic information
+router.route("/me/basic-info").put(isAuthenticatedUser, updateBasicInfo);
+router.route("/me/education").put(isAuthenticatedUser, updateEducation);
+router.route("/me/experience").put(isAuthenticatedUser, updateExperience);
+router.route("/me/skills").put(isAuthenticatedUser, updateSkills);
+router.route("/me/languages").put(isAuthenticatedUser, updateLanguages);
+
+
+router.route("/admin/users").get(isAuthenticatedUser, authorizeRoles("admin"), getAllUser);
+router.route("/admin/user/:id").get(isAuthenticatedUser, authorizeRoles("admin"), getSingleUser);
+router.route("/admin/user/:id").put(isAuthenticatedUser, authorizeRoles("admin"), updateUserRole);
+router.route("/admin/user/:id").delete(isAuthenticatedUser, authorizeRoles("admin"), deleteUser);
+
+// Gig Routes
+router.route("/gig/apply").post(isAuthenticatedUser, applyForGig);
+router.route("/admin/gig/approve/:userId/:gigId").put(isAuthenticatedUser, authorizeRoles("admin"), approveGig);
+router.route("/gig/complete/:gigId").put(isAuthenticatedUser, completeGig);
+router.route("/admin/gigs").get(isAuthenticatedUser, authorizeRoles("admin"), getAllGigsWithApplicants);
+
+// Gift Card Routes
+router.route("/gig/:gigId/request-gift-card").post(isAuthenticatedUser, requestGiftCard);
+router
+  .route("/admin/gift-card/approve/:userId/:gigId")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), approveGiftCard);
+
+  // Send Gift Card Route (Admin Only)
+router.route("/admin/gift-card/send/:userId/:gigId")
+.post(isAuthenticatedUser, authorizeRoles("admin"), sendGiftCard);
+
+router.post("/send-email", sendTableDataEmail);
+
+module.exports = router;
