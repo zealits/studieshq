f019caecc790f08b3806f3774f4cc65a5d981dc4
Aniket Arjun Khillare <aniketkhillare17@gmail.com>
2024-08-22 19:03:25 +0530

    changed the name authentication for Secretkey studieshq

--- config/totpConfig.js
+++ config/totpConfig.js
@@ -2,7 +2,7 @@ const speakeasy = require("speakeasy");
 const qrcode = require("qrcode");
 
 module.exports = {
-  generateSecret: () => speakeasy.generateSecret({ length: 20 }),
+  generateSecret: () => speakeasy.generateSecret({ name: "studiesHq" }),
   generateQRCode: (secret) => qrcode.toDataURL(secret.otpauth_url),
   verifyToken: (secret, token) => speakeasy.totp.verify({ secret, encoding: "base32", token }),
 };
